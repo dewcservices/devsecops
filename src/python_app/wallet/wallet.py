0b4c985247a94dc453106b58a409465822b5ba55
ScottAhern <scottahern96@gmail.com>
2024-05-08 03:57:55 +0000

    feat: python CI tooling + venv caching + Sonar coverage reports

--- src/python_app/wallet/wallet.py
+++ src/python_app/wallet/wallet.py
@@ -9,7 +9,7 @@ class Wallet(object):
 
     def spend_cash(self, amount):
         if self.balance < amount:
-            raise InsufficientAmount('Not enough available to spend {}'.format(amount))
+            raise InsufficientAmount("Not enough available to spend {}".format(amount))
         self.balance -= amount
 
     def add_cash(self, amount):
