f964b5813722dc45a9d7a3f793b1a38f7f4a94e7
GeoDerp <18461782+GeoDerp@users.noreply.github.com>
2025-03-29 19:45:07 +1030

    fix(devcontainer): docker add user, multistage and dev script, rm format on save
    https://github.com/dewcservices/devsecops/pull/29#discussion_r2013264735
    https://github.com/dewcservices/devsecops/pull/29#discussion_r2013259936

---
+++ node-pipeline/.devcontainer/devtools.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+# Install Trivy 
+<<EOF cat >> /etc/yum.repos.d/trivy.repo
+[trivy]
+name=Trivy repository
+baseurl=https://aquasecurity.github.io/trivy-repo/rpm/releases/\$basearch/
+gpgcheck=1
+enabled=1
+gpgkey=https://aquasecurity.github.io/trivy-repo/rpm/public.key
+EOF
+
+dnf -y update-minimal --security --sec-severity=Important --sec-severity=Critical && \
+# Install trivy package
+dnf install trivy -y; \
+# Clean package cache
+dnf clean all
